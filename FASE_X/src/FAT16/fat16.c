f8502cde9055d6d132df70ef7ecdcb061532d405
codevallsma <e.vallsmadella@gmail.com>
2021-05-16 18:53:02 +0200

    REFACTOR: Reformated code and split the readAndCompareIfEqual fat file function into two.
    This split makes it more clear to read and efficient because we were previously comparing filenames even if they were directories, in that case we do not need it.

--- FASE_X/src/FAT16/fat16.c
+++ FASE_X/src/FAT16/fat16.c
@@ -72,7 +72,8 @@ void showFAT16info(int fd) {
 }
 
 /*********************************************** FIND ****************************************************/
-uint num_of_subdirsFAT =0;
+uint num_of_subdirsFAT = 0;
+
 /**
  * Reads the most important parts of the boot sector
  * @param fd : The file descriptor of the volume
@@ -83,7 +84,7 @@ Fat16BootSector readFAT16BootSector(int fd) {
     //FAT_Data data;
     lseek(fd, OFFSET_SYSTEM_NAME, SEEK_SET);
     //setting the \0 delimiter
-    memset(bS.systName, '\0', sizeof (bS.systName));
+    memset(bS.systName, '\0', sizeof(bS.systName));
     read(fd, bS.systName, 8);
     //sector size
     lseek(fd, OFFSET_SECTOR_SIZE, SEEK_SET);
@@ -105,102 +106,105 @@ Fat16BootSector readFAT16BootSector(int fd) {
     read(fd, &bS.BPB_FATsz16, 2);
     //volabel
     lseek(fd, OFFSET_LABEL, SEEK_SET);
-    memset(bS.volLabel, '\0', sizeof (bS.volLabel));
+    memset(bS.volLabel, '\0', sizeof(bS.volLabel));
     read(fd, bS.volLabel, 11);
 
     return bS;
 }
-/**
- * Gets the file size from the directory
- * @param volumeFd : the file descriptor of the filñe
- * @param rootDirSec: The root dir sector in bytes
- * @return the size in bytes of the file
- */
-uint getFileSize(int volumeFd, int rootDirSec){
-    uint size;
-    lseek(volumeFd, rootDirSec + DIRECTORY_OFFSET_DIR_SIZE, SEEK_SET);
-    read(volumeFd,&size, sizeof(uint));;
-    return size;
-}
 
 /**
- *
- * @return
+ * Reads the current directory information
+ * @param volume : the file descriptor of the file system
+ * @param entry : the entry struct
+ * @param dirOffset : The file address to start reading the file
  */
-bool readAndCompareIfEqual(int volume, uint dirOffset, Fat16Entry *entry, char* fileNameToSeek){
-    u_char fullFilename[FILENAME_SIZE+EXTENSION_SIZE+2];
-
+void readDirInfo(int volume, Fat16Entry *entry, uint dirOffset) {
     //setting the pointer of the file to the beggining of the directory
     lseek(volume, dirOffset, SEEK_SET);
     //reading the filename
-    readUntilStatic(volume,entry->filename, FILENAME_SIZE+1, 32);
+    readUntilStatic(volume, entry->filename, FILENAME_SIZE + 1, 32);
     //reading the attribute of the file/directory
-    lseek(volume, dirOffset+DIRECTORY_ATTR_OFFSET, SEEK_SET);
-    read(volume, &(entry->attributes),sizeof(u_char));
+    lseek(volume, dirOffset + DIRECTORY_ATTR_OFFSET, SEEK_SET);
+    read(volume, &(entry->attributes), sizeof(u_char));
     //skipping the bytes that correspond to the filename
-    lseek(volume, dirOffset +FILENAME_SIZE, SEEK_SET);
+    lseek(volume, dirOffset + FILENAME_SIZE, SEEK_SET);
     //reading the extension
-    readUntilStatic(volume,entry->ext,EXTENSION_SIZE+1, 32);
+    readUntilStatic(volume, entry->ext, EXTENSION_SIZE + 1, 32);
     //reading starting cluster
-    lseek(volume, dirOffset+ DIRECTORY_OFFSET_CLUSTER_LO, SEEK_SET);
-    read(volume,&(entry->starting_cluster), 2 );
+    lseek(volume, dirOffset + DIRECTORY_OFFSET_CLUSTER_LO, SEEK_SET);
+    read(volume, &(entry->starting_cluster), 2);
     //reading size
     lseek(volume, dirOffset + DIRECTORY_OFFSET_DIR_SIZE, SEEK_SET);
-    read(volume,&(entry->file_size), sizeof(uint));
+    read(volume, &(entry->file_size), sizeof(uint));
+}
+
+/**
+ * Takes the filename found in the filesystem and commpares it with the one introduced by the user
+ * @param entry : the information of the current file/directory
+ * @param fileNameToSeek : the filename to be searched introduced by the user
+ * @return
+ */
+bool compareFilenames(Fat16Entry *entry, char *fileNameToSeek) {
+    u_char fullFilename[FILENAME_SIZE + EXTENSION_SIZE + 2];
     //check if the file contains ~ meaning the filename is larger than 8 characters
-    if(strstr(entry->filename,"~") != NULL){
+    if (strstr(entry->filename, "~") != NULL) {
         //looking for the position of the array containing ~
-        for(int i =0; i< FILENAME_SIZE+1; i++){
+        for (int i = 0; i < FILENAME_SIZE + 1; i++) {
             //replacing ~ by \0
-            if(entry->filename[i]=='~' && entry->filename[i+1]=='1') entry->filename[i] = '\0';
+            if (entry->filename[i] == '~' && entry->filename[i + 1] == '1') entry->filename[i] = '\0';
         }
     }
     //does the file have extension?
-    if(strlen(entry->ext)>0){
+    if (strlen(entry->ext) > 0) {
         // it has an extension
-        strcpy(fullFilename,entry->filename);
+        strcpy(fullFilename, entry->filename);
         int midaFullname = strlen(fullFilename);
         fullFilename[midaFullname++] = '.';
         fullFilename[midaFullname++] = '\0';
         strcat(fullFilename, entry->ext);
-        return strcmp(fullFilename, fileNameToSeek) ==0;
+        //comparing if filenames are the same including the extension
+        return strcmp(fullFilename, fileNameToSeek) == 0;
     } else {
         // it does not have an extension
-        return strcmp(fileNameToSeek,entry->filename) ==0;
+        //comparing if filenames are the same without the extension
+        return strcmp(fileNameToSeek, entry->filename) == 0;
     }
 }
 
 /**
- *
- * @param fd
- * @param bS
- * @param fileToFind
- * @param rootDirOffset
+ * Seeks for the filename desired by the user in the filesystem
+ * @param fd: the file descriptor of the file system
+ * @param bS: The boot sector information
+ * @param fileToFind: THe filename to search given by the user
+ * @param dirOffset: The address to start reading the file information
  */
-bool seekFile(int fd, char *fileToFind, uint32_t rootDirOffset, Fat16BootSector bS, unsigned int firstDataSector,char *** path){
+bool
+seekFile(int fd, char *fileToFind, uint32_t dirOffset, Fat16BootSector bS, unsigned int firstDataSector, char ***path) {
     Fat16Entry entry;
-    do{
-        bool equal = readAndCompareIfEqual(fd, rootDirOffset, &entry, fileToFind);
+    do {
+        readDirInfo(fd, &entry, dirOffset);
         //if the attribute tells us that it is a file, we have to inspect it to know if it has the same filename as the one given by parameters
-        if(entry.attributes==ATTR_ARCHIVE){
+        if (entry.attributes == ATTR_ARCHIVE) {
             //reading the filename and the extension
-            if(equal){
+            if (compareFilenames(&entry, fileToFind)) {
                 char *buffer;
                 asprintf(&buffer, "\tFitxer trobat. Ocupa %u bytes\n\n", entry.file_size);
                 printaColors(BOLDGREEN, buffer);
                 free(buffer);
                 return true;
             }
-        } else if (entry.attributes==ATTR_DIR && strcmp(entry.filename, ".") &&  strcmp(entry.filename, "..")){
-           if(seekFile(fd, fileToFind, (unsigned  int)((entry.starting_cluster-2)*bS.sectPerCluster*bS.sectorSize) + firstDataSector, bS, firstDataSector, path)){
-               //store file path
-              *path =  copyDirPaths(entry.filename, *path, &num_of_subdirsFAT);
-               return true;
-           }
+        } else if (entry.attributes == ATTR_DIR && strcmp(entry.filename, ".") && strcmp(entry.filename, "..")) {
+            if (seekFile(fd, fileToFind,
+                         (unsigned int) ((entry.starting_cluster - 2) * bS.sectPerCluster * bS.sectorSize) +
+                         firstDataSector, bS, firstDataSector, path)) {
+                //store file path
+                *path = copyDirPaths(entry.filename, *path, &num_of_subdirsFAT);
+                return true;
+            }
         }
         // Get the next directory entry
-        rootDirOffset = rootDirOffset+ DIRECTORY_ENTRY_SIZE;
-    }while(entry.attributes!=0);
+        dirOffset = dirOffset + DIRECTORY_ENTRY_SIZE;
+    } while (entry.attributes != 0);
     return false;
 }
 
@@ -212,18 +216,23 @@ bool seekFile(int fd, char *fileToFind, uint32_t rootDirOffset, Fat16BootSector 
 void FAT_find(int fd, char *fileToFind) {
     //getting all the information from the boot sector
     Fat16BootSector bS = readFAT16BootSector(fd);
+    //calculating the root dir sector value
     unsigned int rootDirSec = (bS.BPB_ResvdSecCnt + (bS.numFats * bS.BPB_FATsz16)) * bS.sectorSize;
-    unsigned int rootDirSectors = (unsigned int) ((bS.maxRootEntries * DIRECTORY_ENTRY_SIZE) + (bS.sectorSize-1)) / bS.sectorSize;
-    unsigned int firstDataSector  = rootDirSec + rootDirSectors*bS.sectorSize;
+    unsigned int rootDirSectors =
+            (unsigned int) ((bS.maxRootEntries * DIRECTORY_ENTRY_SIZE) + (bS.sectorSize - 1)) / bS.sectorSize;
+    unsigned int firstDataSector = rootDirSec + rootDirSectors * bS.sectorSize;
     //setting filename to upper case so we can compare it ignoring cases
     toUpper(fileToFind);
-    char ** path;
+    //declaring the array that will store the path to the file
+    char **path;
     //seek the file to be found
-    if(seekFile(fd,fileToFind, rootDirSec, bS, firstDataSector, &path)){
+    if (seekFile(fd, fileToFind, rootDirSec, bS, firstDataSector, &path)) {
         //file found
-        printPaths(path,fileToFind,num_of_subdirsFAT);
+        printPaths(path, fileToFind, num_of_subdirsFAT);
+        //freeing the dynamic memmory asked
         freePath(path, num_of_subdirsFAT);
     } else {
-        printaColors(BOLDRED,"\tFILE NOT FOUND INSIDE THE FAT16 FILESYSTEM!\n\n");
+        //file not found
+        printaColors(BOLDRED, "\tFILE NOT FOUND INSIDE THE FAT16 FILESYSTEM!\n\n");
     }
 }
